f3ce11e143bbfaf601b81d98fb49b93a402a85e0
yaredghebre <yared2391@gmail.com>
2023-11-28 15:35:55 +0100

    create Tag fixed

--- controllers/tagsController.js
+++ controllers/tagsController.js
@@ -1,16 +1,48 @@
 const prisma = require("../library/PrismaClient");
-// const slugify = require("slugify");
+const slugify = require("slugify");
+const validation = require("../exceptions/ValidationError");
 
-async function store(req, res) {
+const { validationResult } = require("express-validator");
+
+async function store(req, res, next) {
+  const validation = validationResult(req);
   const addData = req.body;
+
+  if (!addData.name) {
+    return next(new Validation("Name missing!"));
+  }
+
+  let slug = slugify(addData.name, {
+    replacement: "-",
+    lower: true,
+  });
+
+  let existingTags = await prisma.tag.findMany({
+    where: {
+      slug: {
+        startsWith: slug,
+      },
+    },
+  });
+
+  const nextSlugCount = existingTags.length;
+
+  if (nextSlugCount > 0) {
+    slug = `${slug}-${nextSlugCount}`;
+  }
+
+  let postsConnection = {};
+  if (addData.posts && Array.isArray(addData.posts)) {
+    postsConnection = {
+      connect: addData.posts.map((post) => ({ id: post })),
+    };
+  }
 
   const newTag = await prisma.tag.create({
     data: {
       name: addData.name,
-      slug: addData.slug,
-      posts: {
-        connect: addData.posts.map((post) => ({ id: post })),
-      },
+      slug: slug,
+      ...postsConnection,
     },
   });
 
